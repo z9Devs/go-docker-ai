23f1e2726e3e892b8d453ebd174bdf7181f889c2
allan <allan.nava@ies-italia.it>
2024-11-22 12:49:34 +0100

    fix: remove unnecessary trailing slash in import path

--- main.go
+++ main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/la-plas-growth/GO-DockerLint-AI/cmd/"
+	"github.com/la-plas-growth/GO-DockerLint-AI/cmd"
 )
 
 func main() {
-if err := cmd.Execute(); err != nil {
+	if err := cmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
