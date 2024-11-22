4819e0d73ae3a4d685a61284003ab3e9285ab3d4
Allan Nava <allannava95@gmail.com>
2024-11-22 14:19:09 +0100

    removed other binaries

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-linux-amd64 .
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-linux-arm64 .
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-windows-amd64 .
-CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-windows-arm64 .
+#CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-linux-arm64 .
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-windows-amd64 .
+#CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -ldflags="-s -w" -a -o go-dockerlint-ai-windows-arm64 .
 #
 mkdir -p /tmp/bin
 pwd
