1fc9d577bffcf6823b9162d3a6f67ea22da08c1e
allan <allan.nava@ies-italia.it>
2024-11-29 16:08:15 +0100

    fix: correct function signature in TestLintDockerfile

--- cmd/service/lint/service_test.go
+++ cmd/service/lint/service_test.go
@@ -3,19 +3,10 @@ package lint_test
 import (
 	"testing"
 
-	wire "github.com/la-plas-growth/go-docker-ai/deps"
 	"github.com/la-plas-growth/go-docker-ai/lib"
 )
 
-func TestLintDockerfile *testing.T) {
+func TestLintDockerfile(t *testing.T) {
 	f := "Dockerfile"
 	t.Log(lib.PrettyPrint(f))
-	return 
-	dockerFileService := wire.InitLint()
-	r, err := dockerFileService.AnalyzeDockerFile(f)
-	if err != nil {
-		t.Errorf("Error: %v", err)
-		return
-	}
-	t.Log(lib.PrettyPrint(r))
 }
